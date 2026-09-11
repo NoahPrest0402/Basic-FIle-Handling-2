ce042fd183f1907a03f7d8febdec6b2777f1461a
NoahPrest0402 <noahprest0402@gmail.com>
2026-09-11 12:47:28 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,23 @@
+const fs = require('fs').promises;
+const path = require('path');
+
+async function loadJSON() {
+    try {
+        const filePath = path.join(__dirname, 'data.json');
+        const jsonData = await fs.readFile(filePath, 'utf8');
+        const data = JSON.parse(jsonData);
+        
+        for (let i = 0; i < 4; i++){
+            console.log("Name:", data. orders[i].name);
+            for (let num = 0; num < 4; num++){
+                console.log(data.products[num].item, "x", data.orders[i].qty[num]); 
+            }
+            console.log("-----------------------")
+        }
+        
+    } catch (err) {
+        console.error('Error reading JSON file:', err.message);
+    }
+}
+
+loadJSON();
